74a12931c3e9590d54e7659395a1281994f55368
Levi-Gabriel <84354292+levi818393922@users.noreply.github.com>
2022-05-31 11:35:44 -0500

    Add files via upload

---
+++ 01-fundamentos-basicos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Fundamentos básicos </title>
+</head>
+
+<body>
+
+<?php
+
+//Fundamentos básicos
+//echo = que serve para imprimir algo na tela
+//esse é o meu primeiro arquivo
+
+echo "Meu nome é Levi <br>";
+print "Minha idade é 17";
+
+?>
+
+</body>
+</html>
